40563455e67001f27bf99d29411571db483c9533
hamimDev <mdhamimhowladerasif@gmail.com>
2024-03-10 00:35:51 +0600

    solve cors problem

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const users = [
+  { id: 1, name: "Hamim", email: "hamim@gmail.com" },
+  { id: 2, name: "Samim", email: "samim@gmail.com" },
+  { id: 3, name: "Jamim", email: "jamim@gmail.com" },
+  { id: 4, name: "Lamim", email: "lamim@gmail.com" },
+];
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("User Management server in running");
 });
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 app.listen(port, () => {
-    console.log(`server is Running on PORT: ${port}`)
+  console.log(`server is Running on PORT: ${port}`);
 });
